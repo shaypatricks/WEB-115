7aceaab002449cd9b53df5a7e583bc15bbb709b6
Steffen Hayes <31227039+shaypatricks@users.noreply.github.com>
2018-10-24 18:14:28 -0500

    Add files via upload

---
+++ javascript/javascript lab/source.js
@@ -0,0 +1,24 @@
+function change01(){
+    document.getElementById("main").src="img/fantasy.jpg";
+    var giant = "The Guardian will protect the winter court all shall fear him."
+   }
+   
+   function change02(){
+       document.getElementById("main").src="img/tree.jpg";
+   }
+   
+   function change03(){
+       document.getElementById("main").src="img/turret-arch.jpg";
+   }
+   
+   function change04(){
+       document.getElementById("main").src="img/winter.jpg";
+   }
+   
+   function change05(){
+       document.getElementById("main").src="img/wolf.jpg";
+   }
+
+   function change06(){
+    document.getElementById("main").src="img/nature.jpg";
+}
